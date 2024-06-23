d6a20a8df2959d7ce21e56b38894b51450a62fa0
Koki Takahashi <hakatasiloving@gmail.com>
2024-06-23 14:04:29 +0000

    Update publication metadata and manuscript content

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,26 +1,20 @@
 module.exports = {
-  title: 'it-quiz', // populated into `publication.json`, default to `title` of the first entry or `name` in `package.json`.
-  author: 'Koki Takahashi <hakatasiloving@gmail.com>', // default to `author` in `package.json` or undefined.
-  // language: 'ja', // default to undefined.
-  // size: 'A4', // paper size.
+  title: 'it-quiz',
+  author: 'Koki Takahashi <hakatasiloving@gmail.com>',
+  language: 'ja',
+  size: 'A5',
   theme: '@vivliostyle/theme-base@^1.0.1', // .css or local dir or npm package. default to undefined.
   entry: [
-    'manuscript.md', // `title` is automatically guessed from the file (frontmatter > first heading).
-    // {
-    //   path: 'epigraph.md',
-    //   title: 'Epigraph', // title can be overwritten (entry > file),
-    //   theme: '@vivliostyle/theme-whatever', // theme can be set individually. default to the root `theme`.
-    // },
-    // 'glossary.html', // html can be passed.
-  ], // `entry` can be `string` or `object` if there's only single markdown file.
+    'manuscript.html', // `title` is automatically guessed from the file (frontmatter > first heading).
+  ],
   // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
-  // output: [ // path to generate draft file(s). default to '{title}.pdf'
-  //   './output.pdf', // the output format will be inferred from the name.
-  //   {
-  //     path: './book',
-  //     format: 'webpub',
-  //   },
-  // ],
+  output: [
+    './it-quiz.pdf',
+    {
+      path: './book',
+      format: 'webpub',
+    },
+  ],
   // workspaceDir: '.vivliostyle', // directory which is saved intermediate files.
   // toc: true, // whether generate and include ToC HTML or not, default to 'false'.
   // cover: './cover.png', // cover image. default to undefined.
